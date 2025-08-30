f4ca46f7ca7e30a256f90ecfaea63e6a530e7451
Fara <fairy2012007@gmail.com>
2025-08-30 16:41:44 +0000

    Oops

--- app.py
+++ app.py
@@ -1,1 +1,20 @@
-print('Hello World1')
+class BankAccount:
+    def __init__(self, owner, balance):
+        self.owner = owner
+        self.balance = balance
+
+    def deposit(self, amount):
+        self.balance += amount
+        print(f"{amount} deposited. New balance = {self.balance}")
+
+    def withdraw(self, amount):
+        if amount <= self.balance:
+            self.balance -= amount
+            print(f"{amount} withdrawn. Balance left = {self.balance}")
+        else:
+            print("Not enough balance!")
+
+acc = BankAccount("Farah", 1000)
+acc.deposit(500)
+acc.withdraw(300)
+acc.withdraw(1500)
