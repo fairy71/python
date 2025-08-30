d5dbfc7b97af6d059ff76589e3110825cd01f437
Fara <fairy2012007@gmail.com>
2025-08-30 16:48:50 +0000

    Oops1

---
+++ Python.py
@@ -0,0 +1,12 @@
+class Calculator:
+    def add(self, a, b):
+        return a + b
+
+    def multiply(self, a, b):
+        return a * b
+
+c = Calculator()
+print("Add:", c.add(5, 3))
+print("Multiply:", c.multiply(4, 2))
+
+
